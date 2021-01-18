8c9505bbf9bc5decb0f4e3b03489e4e72d416806
naf <naf@auri.ga>
2021-01-18 14:23:09 +0900

    add model

--- sns/post/models.py
+++ sns/post/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    content = models.CharField(max_length=5000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at',)
