22171e4f0d02efc6290ca30d749ae24ca742b3a9
naf <naf@auri.ga>
2021-01-19 09:35:26 +0900

    Change ja

--- sns/sns/settings.py
+++ sns/sns/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
-    'post'
+    'account.apps.AccountConfig',
+    'post.apps.PostConfig'
 ]
 
 MIDDLEWARE = [
