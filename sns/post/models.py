6c9ab610398be685dfe820caf9fb04e0bc84ef93
naf <naf@auri.ga>
2021-01-30 15:13:59 +0900

    add fav model

--- sns/post/models.py
+++ sns/post/models.py
@@ -10,6 +10,17 @@ class Post(models.Model):
     )
     content = models.CharField(max_length=5000)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    favorite_count = models.IntegerField(default=0)
     class Meta:
         ordering = ('-created_at',)
+
+class Favorite(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='favorite_owner'
+    )
+    content_id = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE
+    )
