6bd1f498876d8fcc441ed2c33d503d58730d15df
naf <naf@auri.ga>
2021-04-18 17:33:23 +0900

    add follow model

--- sns/account/models.py
+++ sns/account/models.py
@@ -1,8 +1,23 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
 class Account(AbstractUser):
     avator = models.ImageField(
         upload_to = '',
         null = True
+    )
+    following_count = models.IntegerField(default=0)
+    follower_count = models.IntegerField(default=0)
+
+class Follow(models.Model):
+    follow_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='follow_owner'
+    )
+    follower_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
     )
