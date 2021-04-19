38961055141e5f93f1509fbd7381d23657df45d1
naf <naf@auri.ga>
2021-04-19 18:55:51 +0900

    urlを修正

--- sns/post/urls.py
+++ sns/post/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:num>', views.index, name='index'),
-    path('post/<int:pk>', views.post_detail, name='detail'),
+    path('timeline/', views.home_timeline, name='home_timeline'),
+    path('timeline/<int:num>', views.home_timeline, name='home_timeline'),
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
     path('post/<int:pk>/delete', views.post_delete, name='delete'),
     path('post/create/', views.create_post, name='create'),
     path('post/<int:pk>/favorite', views.post_favorite, name='favorite')
