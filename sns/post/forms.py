e08208ca904947e3292df2ac1e01451d1b9e32b0
Naf <naf@auri.ga>
2021-05-23 19:16:40 +0900

    画像を添付できるように

--- sns/post/forms.py
+++ sns/post/forms.py
@@ -3,4 +3,5 @@ from django import forms
 class PostForm(forms.Form):
     content = forms.CharField(max_length=5000, 
     widget=forms.Textarea(attrs={'class': 'form-control'}))
+    image = forms.ImageField(label='画像', required=False)
 
